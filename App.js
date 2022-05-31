d1992a247c9b37d54137dbe2cb9f875c42a0ac27
Henry Quartey <henryfoli36@gmail.com>
2022-05-31 14:44:39 +0000

    New Commit

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Men from './components/Men'
+import Women from './components/Women'
+import product from './components/product'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Men />
+      {/* <Women /> */}
+      {/* <Product /> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
+    backgroundColor: '#efefe9',
     alignItems: 'center',
     justifyContent: 'center',
   },
